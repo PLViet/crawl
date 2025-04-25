debf831ecc4a4a996ad4a4312539784eca570c55
Việt Lê <43601619+PLViet@users.noreply.github.com>
2025-04-25 15:53:56 +0700

    Update index.py
    Signed-off-by: Việt Lê <43601619+PLViet@users.noreply.github.com>

--- index.py
+++ index.py
@@ -1,27 +1,18 @@
-import asyncio
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+import csv
 
+url = "http://books.toscrape.com"
+res = requests.get(url)
+soup = BeautifulSoup(res.text, "html.parser")
+books = soup.select(".product_pod")
 
-async def create_driver():
-    firefox_options = Options()
-    firefox_options.headless = False
-    return webdriver.Firefox(options=firefox_options) 
+with open("books.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Tên", "Giá", "Đánh giá"])
 
-async def start():
-    driver = await create_driver()
-
-    driver.get("https://www.facebook.com/hunggcom003/posts/pfbid02k8L3HbD9z3KrYWFhHd9DZU7WWM8e8UpF3k9g7UsuDuStj9Djq2koCXnvTRC4dRskl")
-    await asyncio.sleep(4)
-    name_page = driver.find_element(By.XPATH, '//*[@data-ad-rendering-role="profile_name"]/h3/span/span/a/strong/span').text
-    title = driver.find_element(By.XPATH, '//*[@data-ad-comet-preview="message"]').text
-    image = driver.find_element(By.XPATH, '//*[@id="«r5»"]/div[1]/a/div[1]/div/div/div/img').get_attribute('src')
-    print("""
-    name_page: {}
-    title: {}
-    image: {}
-    """.format(name_page, title, image))
-
-if __name__ == '__main__':
-    asyncio.run(start())
+    for book in books:
+        title = book.h3.a["title"]
+        price = book.select_one(".price_color").text
+        rating = book.p["class"][1]
+        writer.writerow([title, price, rating])
